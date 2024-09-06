9b0a93466132e1eecf1aab4da4575c52feae2838
Irham Dzuhri <irhamdz@gmail.com>
2024-09-06 09:58:02 +0700

    improve visual

--- bot/config/config.py
+++ bot/config/config.py
@@ -5,8 +5,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
     API_KEY: str
 
-    POINTS: list[int] = [500, 800]
-    SWIPE_POINTS: list[int] = [950, 1100]
+    POINTS: list[int] = [700, 900]
+    SWIPE_POINTS: list[int] = [2500, 3000]
 
     USE_REF: bool = False
     REF_ID: str = ""
